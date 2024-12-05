a9ac08ee428f9a6611b3a730cf280231a0eab0ae
Bethel Eyo <betheleyo@gmail.com>
2024-12-05 18:20:12 +0000

    Add Skeleton to Home Page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -3,16 +3,25 @@ import FilterParams from "../../components/FilterParams/FilterParams";
 import GiphyListItem from "../../components/GiphyListItem/GiphyListItem";
 import useHomePageLogic from "./useHomePageLogic";
 import Pagination from "../../components/Pagination/Pagination";
+import Skeleton from "../../components/Skeleton/Skeleton";
 
 const HomePage = () => {
-  const { trendingGifs, page, setPage } = useHomePageLogic();
+  const { trendingGifs, page, setPage, isLoading } = useHomePageLogic();
 
   return (
     <div>
       <FilterParams />
       <Pagination page={page} setPage={setPage} />
       <div className="mt-3 columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2">
-        {trendingGifs.map((trendingGif: IGif) => (
+        {isLoading
+          ? Array(10) // Same number as GIFs per page
+              .fill(0)
+              .map((_, index) => (
+                <div key={index} className="h-32 w-full">
+                  <Skeleton height="100%" baseColor="#e0e0e0" highlightColor="#f5f5f5" />
+                </div>
+              ))
+          : trendingGifs.map((trendingGif: IGif) => (
           <GiphyListItem trendingGif={trendingGif} key={trendingGif.id} />
         ))}
       </div>
--- src/pages/Home/useHomePageLogic.ts
+++ src/pages/Home/useHomePageLogic.ts
@@ -5,9 +5,11 @@ const useHomePageLogic = () => {
   const { giphyFetch, trendingGifs, setTrendingGifs, filter } = useGiphyContext();
   const [page, setPage] = useState(0);
   const gifsPerPage = 10;
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchTrendingGifs = useCallback(async () => {
     try {
+      setIsLoading(true);
       const { data } = await giphyFetch.trending({
         limit: gifsPerPage,
         offset: page * gifsPerPage,
@@ -16,15 +18,19 @@ const useHomePageLogic = () => {
       setTrendingGifs(data);
     } catch (error) {
       console.debug(error);
+    } finally {
+      setIsLoading(false);
     }
   }, [filter, giphyFetch, page, setTrendingGifs]);
-
 
   useEffect(() => {
     fetchTrendingGifs();
   }, [fetchTrendingGifs, filter]);
 
-  return useMemo(() => ({ trendingGifs, page, setPage }), [trendingGifs, page, setPage]);
+  return useMemo(
+    () => ({ trendingGifs, page, setPage, isLoading }),
+    [trendingGifs, page, setPage, isLoading]
+  );
 };
 
 export default useHomePageLogic;
