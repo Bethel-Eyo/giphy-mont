f9cf176d635995cb5fd2a352e2c33e90905792f0
Bethel Eyo <betheleyo@gmail.com>
2024-12-06 12:22:18 +0000

    Add unit tests for Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,47 +1,26 @@
-import { beforeEach, describe, expect, test, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Header from "./Header";
-import { GiphyContextType } from "../../contexts/Giphy/GiphyContext";
-import { GiphyFetch } from "@giphy/js-fetch-api";
-import * as useHeaderLogic from "./useHeaderLogic";
-import { GiphyProvider } from "../../contexts/Giphy/GiphyProvider";
 import { BrowserRouter } from "react-router-dom";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import { GiphyProvider } from "../../contexts/Giphy/GiphyProvider";
+import Header from "./Header";
+import * as useHeaderLogic from "./useHeaderLogic";
 
-const mockGiphyFetch = {
-  trending: vi.fn(), // Mock only the methods needed
-} as unknown as GiphyFetch;
-
-// Mocked context values
-const mockContextValue: GiphyContextType = {
-  giphyFetch: mockGiphyFetch,
-  trendingGifs: [],
-  setTrendingGifs: vi.fn(),
-  favorites: [],
-  filter: "gifs",
-  setFilter: vi.fn(),
-  saveToFavorites: vi.fn(),
-};
+const mockCategories = [
+  { name: "Action", name_encoded: "encoded_action", subcategories: [] },
+  { name: "Comedy", name_encoded: "encoded_comedy", subcategories: [] },
+];
 
 const useHeaderLogicReturnValue: ReturnType<typeof useHeaderLogic.default> = {
-  categories: [],
+  categories: mockCategories,
   favorites: ["1", "2"],
   showCategories: false,
   setShowCategories: vi.fn(),
 };
 
-beforeEach(() => {
-  // vi.spyOn(GiphyContext, 'useContext').mockImplementation(() => mockContextValue)
-//   vi.spyOn(useHeaderLogic, "default").mockImplementation(() => {
-//     return useHeaderLogicReturnValue;
-//   });
-});
-
-// Mocked GiphyProvider for wrapping the Header component
-// const MockGiphyProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-//   return <GiphyContext.Provider value={mockContextValue}>{children}</GiphyContext.Provider>;
-// };
-
 describe("Header Component", () => {
+  beforeEach(() => {
+    vi.spyOn(useHeaderLogic, "default").mockReturnValue(useHeaderLogicReturnValue);
+  });
   test("renders the title 'GIPHY-MONT'", () => {
     render(
       <GiphyProvider>
@@ -51,5 +30,48 @@ describe("Header Component", () => {
       </GiphyProvider>
     );
     expect(screen.getByText("GIPHY-MONT")).toBeInTheDocument();
+  });
+
+  test("displays the first 5 categories in the header", () => {
+    render(
+      <GiphyProvider>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </GiphyProvider>
+    );
+    mockCategories.forEach((category) => {
+      const categoryLink = screen.getByText(category.name);
+      expect(categoryLink).toBeInTheDocument();
+    });
+  });
+
+  test("displays the favorite link if there are favorites", () => {
+    render(
+      <GiphyProvider>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </GiphyProvider>
+    );
+    const favoriteLink = screen.getByText(/favorite gifs/i);
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  test("does not render the favorites link if there are no favorites", () => {
+    vi.spyOn(useHeaderLogic, "default").mockReturnValue({
+      ...useHeaderLogicReturnValue,
+      favorites: [],
+    });
+
+    render(
+      <GiphyProvider>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </GiphyProvider>
+    );
+    const favoriteLink = screen.queryByText(/favorite gifs/i);
+    expect(favoriteLink).not.toBeInTheDocument();
   });
 });
