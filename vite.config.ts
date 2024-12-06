316d38088366448c1f986888cb829b420450f154
Bethel Eyo <betheleyo@gmail.com>
2024-12-06 11:18:03 +0000

    Setup vitest for unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    setupFiles: "src/setupTests.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["lcov", "html", "text"],
+      // Make sure to add the exclude list to sonar project properties file
+      exclude: [
+        "**/**/*.test.{ts,tsx}",
+        "**/**/*.d.{ts,tsx}",
+        "src/setupTests.ts",
+        "eslint.config.js",
+      ],
+    },
+  },
+});
