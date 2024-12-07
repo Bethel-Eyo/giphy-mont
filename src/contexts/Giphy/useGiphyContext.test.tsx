de51f7945cc76ac989cd0a33ba1438130eb89072
Bethel Eyo <betheleyo@gmail.com>
2024-12-07 05:27:24 +0000

    minor preference change

--- src/contexts/Giphy/useGiphyContext.test.tsx
+++ src/contexts/Giphy/useGiphyContext.test.tsx
@@ -1,17 +1,17 @@
 import { GiphyFetch } from "@giphy/js-fetch-api";
 import { renderHook } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { GiphyContext, GiphyContextType } from "./GiphyContext";
 import { useGiphyContext } from "./useGiphyContext";
 
 describe("useGiphyContext", () => {
-  it("throws an error when used outside GiphyProvider", () => {
+  test("throws an error when used outside GiphyProvider", () => {
     expect(() => {
       renderHook(() => useGiphyContext());
     }).toThrow("useGiphyContext must be used within a GiphyProvider");
   });
 
-  it("provides context data when used within GiphyProvider", () => {
+  test("provides context data when used within GiphyProvider", () => {
     const mockContextValue: GiphyContextType = {
       giphyFetch: {} as GiphyFetch,
       trendingGifs: [],
